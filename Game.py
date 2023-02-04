21b3bf93f5b66cf49f9acd01b94e4e6ac151eab2
Casey Sytsema <sytsemacj@gmail.com>
2023-02-04 14:16:11 -0500

    Collision working

--- Game.py
+++ Game.py
@@ -12,27 +12,51 @@ SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 640
 
 class Player(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game, velocity=8):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface([30, 30])
         self.image.fill((0, 0, 255))
         self.rect = self.image.get_rect()
         self.x = 0
         self.y = 0
+        self.game = game
+        self.velocity = velocity
 
     def update(self):
         keys = pg.key.get_pressed()
-        if keys[pg.K_w] and self.y > 0:
-            self.y -= 8
-        if keys[pg.K_a] and self.x > 0:
-            self.x -= 8
-        if keys[pg.K_s] and self.y + self.rect.height < SCREEN_HEIGHT:
-            self.y += 8
-        if keys[pg.K_d] and self.x + self.rect.width < SCREEN_WIDTH:
-            self.x += 8
-        
+        new_x, new_y = self.x, self.y
+
+        if keys[pg.K_w]: new_y -= self.velocity
+
+        if keys[pg.K_a]: new_x -= self.velocity
+
+        if keys[pg.K_s]: new_y += self.velocity
+
+        if keys[pg.K_d]: new_x += self.velocity
+
+        if self.collides_with_black(new_x, new_y):
+            return
+
+        if new_x < 0:
+            new_x = 0
+        if new_y < 0:
+            new_y = 0
+        if new_x + self.rect.width > SCREEN_WIDTH:
+            new_x = SCREEN_WIDTH - self.rect.width
+        if new_y + self.rect.height > SCREEN_HEIGHT:
+            new_y = SCREEN_HEIGHT - self.rect.height
+
+        self.x, self.y = new_x, new_y
         self.rect.x = self.x
         self.rect.y = self.y
+
+    def collides_with_black(self, x, y):
+        new_position = pg.Rect(x, y, 30, 30)
+        for cell_rect in self.game.walls:
+            if new_position.colliderect(cell_rect):
+                print("collision detected")
+                return True
+        return False
 class Game:
     def __init__(self):
         self.board = []
@@ -48,9 +72,10 @@ class Game:
         self.finished = False
         self.running = False
 
-        self._cells = self.create_cells()
+        self._cells: list[pg.Rect] = self.create_cells()
         self._delay = 250
-        self.player = Player()
+        self.player = Player(self)
+        self.walls = []
 
         pg.init()
         self._display = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
@@ -58,9 +83,10 @@ class Game:
     def loop(self):
         clock = pg.time.Clock()
         self.create_maze()
-
+        self.walls = self.get_walls()
         while not self.finished:
             delta = clock.tick(FPS) / 1000.0 # set fps
+            self.walls = self.get_walls()
 
             for event in pg.event.get():
                 # if the user hits the X
@@ -156,3 +182,16 @@ class Game:
         self.start_random()
         for _ in range(100):
             self.update()
+
+    def cell_at_pos(self, x, y):
+        col = x // 32
+        row = y // 32
+        return row, col
+
+    def get_walls(self) -> list[pg.Rect]:
+        walls = []
+        for i in range(SIZE):
+            for j in range(SIZE):
+                if self.board[i][j] == ALIVE:
+                    walls.append(self._cells[i][j])
+        return walls
