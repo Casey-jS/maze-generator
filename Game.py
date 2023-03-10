4617ea2a91ec804cef18af4cfe118f1167c2e3fc
Casey Sytsema <sytsemacj@gmail.com>
2023-03-09 20:16:41 -0500

    Final

--- Game.py
+++ Game.py
@@ -28,42 +28,53 @@ class Game:
         self.walls = []
         self.ball = player.Ball(self)
 
-
         pg.init()
         self._display = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
+
     def loop(self):
+        '''
+        main loop function
+        - ticks the clock
+        - updates the walls based on clicks
+        - handles click events and updates player position
+        - updates the display
+        '''
         clock = pg.time.Clock()
+        time_step = 1 / FPS # calculate the time step based on the desired fps
+        accumulated_time = 0
         while not self.finished:
             self.walls = self.get_walls() # to get changes based on clicked cells
             if self.check_win():
                 self.game_over() # spawns end game window
 
-            delta = clock.tick(FPS) / 1000.0 # set fps
-
-            for event in pg.event.get():
-                # if the user hits the X
-                if event.type == pg.QUIT:
-                    self.finished = True
-
-                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-                    coords = pg.mouse.get_pos()
-
-                    row, col = self.cell_clicked(coords) 
-                    if row is not None:
-                        self.change_color(row, col)
-
-            self.player.update()
-
-            if self.running:
-                pg.time.wait(self._delay)
+            delta_time = clock.tick(FPS) / 1000.0 # delta time in seconds
+            accumulated_time += delta_time
             
+            while accumulated_time >= time_step:
+                for event in pg.event.get():
+                    # if the user hits the X
+                    if event.type == pg.QUIT:
+                        self.finished = True
+
+                    if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                        coords = pg.mouse.get_pos()
+
+                        row, col = self.cell_clicked(coords) 
+                        if row is not None:
+                            self.change_color(row, col)
+
+                self.player.update(time_step)
+                accumulated_time -= time_step
+
             self._display.fill((255, 255, 255))
             self.draw_cells()
             self._display.blit(self.player.image, self.player.rect) # draw player
             self._display.blit(self.ball.image, self.ball.rect) # draw ball
             pg.display.update()
+            clock.tick(FPS)
         pg.quit()
+
 
     # returns a list of pg.Rects that represent the maze
     def create_cells(self):
@@ -82,7 +93,7 @@ class Game:
                     return row, col
         return None, None
 
-    # change cell to opposite color when clicked
+    # change cell to opposite color when clicked, based on the row and column
     def change_color(self, row, col):
         cell = self.board[row][col]
         if cell == DEAD:
@@ -110,48 +121,56 @@ class Game:
         return self.ball.rect.colliderect(self.player.rect)
 
     '''
-    creates a new window that allows the user to either exit (closes all window),
+    creates a new window that allows the user to either exit (closes all windows),
     or start a new game with a new maze
     '''
     def game_over(self):
-        pg.init()
+
+        # initialize new display
+        pg.init() 
         game_over_display = pg.display.set_mode((300, 100))
         pg.display.set_caption("Game Over")
         font = pg.font.Font(None, 36)
+
+        # render text
         text = font.render("You won!", True, (255, 255, 255))
         text_rect = text.get_rect()
         text_rect.centerx = game_over_display.get_rect().centerx
         text_rect.centery = game_over_display.get_rect().centery - 30
         game_over_display.blit(text, text_rect)
 
-        font = pg.font.Font(None, 20)
+        # update font size
+        font = pg.font.Font(None, 20) 
         exit_text = font.render("Exit", True, (0, 0, 0))
         new_game_text = font.render("New Game", True, (0, 0, 0))
 
+        # create buttons
         exit_button = pg.Rect(50, 50, 75, 30)
         new_game_button = pg.Rect(150, 50, 75, 30)
 
+        # while we are waiting for user choice
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
-                    sys.exit()
+                    sys.exit() # exit both windows if user hits exits out
                 if event.type == pg.MOUSEBUTTONDOWN:
                     if exit_button.collidepoint(event.pos):
                         pg.quit()
-                        sys.exit()
+                        sys.exit() # exit both windows
                     elif new_game_button.collidepoint(event.pos):
                         pg.quit()
                         game = Game()
                         game.loop()
                         sys.exit()
 
+                        
+            # draw buttons and text
             pg.draw.rect(game_over_display, (255, 255, 255), exit_button)
             pg.draw.rect(game_over_display, (255, 255, 255), new_game_button)
             exit_text_rect = exit_text.get_rect()
             exit_text_rect.center = (exit_button.x + exit_button.width // 2, exit_button.y + exit_button.height // 2)
-            game_over_display.blit(exit_text, exit_text_rect)
-            
+            game_over_display.blit(exit_text, exit_text_rect)   
             new_game_text_rect = new_game_text.get_rect()
             new_game_text_rect.center = (new_game_button.x + new_game_button.width // 2, new_game_button.y + new_game_button.height // 2)
             game_over_display.blit(new_game_text, new_game_text_rect)
--- player.py
+++ player.py
@@ -1,6 +1,5 @@
 import pygame as pg
 
-
 SCREEN_HEIGHT = 640
 SCREEN_WIDTH = 640
 
@@ -13,8 +12,16 @@ class Ball(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = SCREEN_WIDTH - self.rect.width
         self.rect.y = SCREEN_HEIGHT - self.rect.height
+
+'''
+the player class
+- holds the player's position
+- sets their velocity
+- holds the game that the player will be in
+- creates the player's rectangle
+'''
 class Player(pg.sprite.Sprite):
-    def __init__(self, game, velocity=4):
+    def __init__(self, game, velocity=128):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load('eevee2.png')
         self.image = pg.transform.scale(self.image, (30, 30))
@@ -24,34 +31,32 @@ class Player(pg.sprite.Sprite):
         self.game = game
         self.velocity = velocity
 
-    def update(self):
+    def update(self, time_step):
         keys = pg.key.get_pressed()
         new_x, new_y = self.x, self.y
 
-        if keys[pg.K_w]: new_y -= self.velocity
-
-        if keys[pg.K_a]: new_x -= self.velocity
-
-        if keys[pg.K_s]: new_y += self.velocity
-
-        if keys[pg.K_d]: new_x += self.velocity
+        # set temporary values for the new position
+        if keys[pg.K_w]: new_y -= self.velocity * time_step
+        if keys[pg.K_a]: new_x -= self.velocity * time_step
+        if keys[pg.K_s]: new_y += self.velocity * time_step
+        if keys[pg.K_d]: new_x += self.velocity * time_step
 
         if self.collides_with_black(new_x, new_y):
             return # do nothing
+        
+        # don't allow player to go off screen
+        if new_x < 0: new_x = 0
+        if new_y < 0: new_y = 0
+        if new_x + self.rect.width > SCREEN_WIDTH: new_x = SCREEN_WIDTH - self.rect.width
+        if new_y + self.rect.height > SCREEN_HEIGHT: new_y = SCREEN_HEIGHT - self.rect.height
 
-        if new_x < 0:
-            new_x = 0
-        if new_y < 0:
-            new_y = 0
-        if new_x + self.rect.width > SCREEN_WIDTH:
-            new_x = SCREEN_WIDTH - self.rect.width
-        if new_y + self.rect.height > SCREEN_HEIGHT:
-            new_y = SCREEN_HEIGHT - self.rect.height
-
+        # else, update player position based on new temp values
         self.x, self.y = new_x, new_y
         self.rect.x = self.x
         self.rect.y = self.y
 
+    # params: new (temp) player position
+    # returns: true if the player collided with a wall
     def collides_with_black(self, x, y):
         new_position = pg.Rect(x, y, 30, 30)
         for cell_rect in self.game.walls:
